f2e840f880e3ffcf2a6181c724def33f7a941ac3
manuel <manuelcarloslopes@icloud.com>
2020-07-04 00:21:30 +0200

    ADD use Swift-numerics instead of FloatingPoint type

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 
 import PackageDescription
 
@@ -13,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "0.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,7 +20,9 @@ let package = Package(
         // depends on.
         .target(
             name: "Easing",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Numerics", package: "swift-numerics"),
+            ]),
         .testTarget(
             name: "EasingTests",
             dependencies: ["Easing"])
--- Tests/EasingTests/EasingTests.swift
+++ Tests/EasingTests/EasingTests.swift
@@ -4,13 +4,16 @@
 //  Created by Manuel Lopes on 20.03.2018.
 //
 
+import Numerics
 import XCTest
 
 @testable import Easing
 
 final class EasingTests: XCTestCase {
 
-    let testArray: [Float] = Array(stride(from: 0, to: 1, by: 0.01))
+    let arrayOfFloats:   [Float]   = Array(stride(from: 0, to: 1, by: 0.01))
+    let arrayOfFloat80s: [Float80] = Array(stride(from: 0, to: 1, by: 0.01))
+    let arrayOfDoubles:  [Double]  = Array(stride(from: 0, to: 1, by: 0.01))
 
     // MARK: - Quadratic
 
@@ -26,9 +29,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_quadratic_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.quadratic.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.quadratic.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.quadratic.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quadratic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quadratic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quadratic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quadratic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quadratic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quadratic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quadratic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quadratic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quadratic.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Cubic
@@ -45,9 +56,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_cubic_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.cubic.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.cubic.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.cubic.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.cubic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.cubic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.cubic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.cubic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.cubic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.cubic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.cubic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.cubic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.cubic.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Quartic
@@ -64,9 +83,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_quartic_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.quartic.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.quartic.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.quartic.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quartic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quartic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quartic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quartic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quartic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quartic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quartic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quartic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quartic.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Quintic
@@ -83,9 +110,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_quintic_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.quintic.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.quintic.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.quintic.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quintic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quintic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.quintic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quintic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quintic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.quintic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quintic.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quintic.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.quintic.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Sine
@@ -102,9 +137,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_sine_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.sine.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.sine.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.sine.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.sine.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.sine.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.sine.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.sine.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.sine.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.sine.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.sine.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.sine.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.sine.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Circular
@@ -121,9 +164,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_circular_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.circular.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.circular.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.circular.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.circular.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.circular.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.circular.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.circular.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.circular.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.circular.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.circular.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.circular.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.circular.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Exponencial
@@ -140,9 +191,17 @@ final class EasingTests: XCTestCase {
     }
 
     func test_exponential_is_strictly_ascending() {
-        XCTAssertTrue(testArray.map { Curve.exponential.easeIn($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.exponential.easeOut($0) }.isOrdered(<))
-        XCTAssertTrue(testArray.map { Curve.exponential.easeInOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.exponential.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.exponential.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloats.map { Curve.exponential.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.exponential.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.exponential.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfFloat80s.map { Curve.exponential.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertTrue(arrayOfDoubles.map { Curve.exponential.easeIn($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.exponential.easeOut($0) }.isOrdered(<))
+        XCTAssertTrue(arrayOfDoubles.map { Curve.exponential.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Elastic
@@ -158,10 +217,18 @@ final class EasingTests: XCTestCase {
         XCTAssertEqual(Curve.elastic.easeInOut(1), 1)
     }
 
-    func test_elastic_is_strictly_ascending() {
-        XCTAssertFalse(testArray.map { Curve.elastic.easeIn($0) }.isOrdered(<))
-        XCTAssertFalse(testArray.map { Curve.elastic.easeOut($0) }.isOrdered(<))
-        XCTAssertFalse(testArray.map { Curve.elastic.easeInOut($0) }.isOrdered(<))
+    func test_elastic_is_not_strictly_ascending() {
+        XCTAssertFalse(arrayOfFloats.map { Curve.elastic.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloats.map { Curve.elastic.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloats.map { Curve.elastic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.elastic.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.elastic.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.elastic.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertFalse(arrayOfDoubles.map { Curve.elastic.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfDoubles.map { Curve.elastic.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfDoubles.map { Curve.elastic.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Back
@@ -177,10 +244,18 @@ final class EasingTests: XCTestCase {
         XCTAssertEqual(Curve.back.easeInOut(1), 1)
     }
 
-    func test_back_is_strictly_ascending() {
-        XCTAssertFalse(testArray.map { Curve.back.easeIn($0) }.isOrdered(<))
-        XCTAssertFalse(testArray.map { Curve.back.easeOut($0) }.isOrdered(<))
-        XCTAssertFalse(testArray.map { Curve.back.easeInOut($0) }.isOrdered(<))
+    func test_back_is_not_strictly_ascending() {
+        XCTAssertFalse(arrayOfFloats.map { Curve.back.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloats.map { Curve.back.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloats.map { Curve.back.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.back.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.back.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.back.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertFalse(arrayOfDoubles.map { Curve.back.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfDoubles.map { Curve.back.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfDoubles.map { Curve.back.easeInOut($0) }.isOrdered(<))
     }
 
     // MARK: - Bounce
@@ -196,9 +271,17 @@ final class EasingTests: XCTestCase {
         XCTAssertTrue(0...1.0000001 ~= Curve.bounce.easeInOut(1))
     }
 
-    func test_bounce_is_strictly_ascending() {
-        XCTAssertFalse(testArray.map { Curve.bounce.easeIn($0) }.isOrdered(<))
-        XCTAssertFalse(testArray.map { Curve.bounce.easeOut($0) }.isOrdered(<))
-        XCTAssertFalse(testArray.map { Curve.bounce.easeInOut($0) }.isOrdered(<))
+    func test_bounce_is_not_strictly_ascending() {
+        XCTAssertFalse(arrayOfFloats.map { Curve.bounce.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloats.map { Curve.bounce.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloats.map { Curve.bounce.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.bounce.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.bounce.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfFloat80s.map { Curve.bounce.easeInOut($0) }.isOrdered(<))
+
+        XCTAssertFalse(arrayOfDoubles.map { Curve.bounce.easeIn($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfDoubles.map { Curve.bounce.easeOut($0) }.isOrdered(<))
+        XCTAssertFalse(arrayOfDoubles.map { Curve.bounce.easeInOut($0) }.isOrdered(<))
     }
 }
