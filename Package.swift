d6f1f8814c594a44e57b2d11ed66a834657cd40c
manuel <manuelcarloslopes@icloud.com>
2020-08-29 18:08:17 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@
 import PackageDescription
 
 let package = Package(name: "Easing",
+                      
                       // Products define the executables and libraries produced by a package, and make them visible to other packages.
                       products: [.library(name: "Easing", targets: ["Easing"])],
 
