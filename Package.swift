7350718d3375e5a5911b48096557599741a21a4e
Manuel Carlos <manuelcarloslopes@icloud.com>
2022-01-23 10:26:49 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,15 +3,11 @@
 import PackageDescription
 
 let package = Package(name: "Easing",
-                      // Products define the executables and libraries produced by a package, and make them visible to other packages.
+ 
                       products: [.library(name: "Easing", targets: ["Easing"])],
 
-                      // Dependencies declare other packages that this package depends on.
                       dependencies: [.package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")],
 
-                      // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-                      // Targets can depend on other targets in this package, and on products in packages which this package
-                      // depends on.
                       targets: [.target(name: "Easing",
                                         // 06 Jul 2020 - No need to use `.product(name: "Numerics", package: "swift-numerics")` because
                                         // we only need the `Real` protocol.
