fe3a22ab038580ff8eddca0638ef6d474505beb3
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-03-15 14:06:46 +0100

    Update and clean up package manifest file.

--- Package.swift
+++ Package.swift
@@ -3,13 +3,23 @@
 import PackageDescription
 
 let package = Package(name: "Easing",
-                      products: [.library(name: "Easing", targets: ["Easing"])],
-                      dependencies: [.package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")],
-                      targets: [.target(name: "Easing",
-                                        // 06 Jul 2020 - No need to use `.product(name: "Numerics", package: "swift-numerics")` because
-                                        // we only need the ``Real`` protocol.
-                                        // Moreover, the `Numerics` product includes the `ComplexModule` which is not yet supported in the
-                                        // latest Ubuntu version.
-                                        dependencies: [.product(name: "RealModule", package: "swift-numerics")]),
-                                .testTarget(name: "EasingTests", dependencies: ["Easing"])],
-                      swiftLanguageVersions: [.v5])
+
+                      products: [
+                        .library(name: "Easing", targets: ["Easing"])
+                      ],
+
+                      dependencies: [
+                        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")
+                      ],
+
+                      targets: [
+                        .target(name: "Easing",
+                                // 06 Jul 2020 - No need to use `.product(name: "Numerics", package: "swift-numerics")` because
+                                // we only need the ``Real`` protocol.
+                                // Moreover, the `Numerics` product includes the `ComplexModule` which is not yet supported in the
+                                // latest Ubuntu version.
+                                dependencies: [.product(name: "RealModule", package: "swift-numerics")]
+                               ),
+                        .testTarget(name: "EasingTests", dependencies: ["Easing"])
+                      ]
+)
